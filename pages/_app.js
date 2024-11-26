08b0bde2a61926a2e48dd5851a928a99e84490f6
Nick Romanidis <nick.romanidis@senecacollege.ca>
2024-11-26 08:25:22 -0500

    Messed up the app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
+
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <br /><Component {...pageProps} />
 }
